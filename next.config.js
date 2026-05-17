822b459adbfe3bb594359193d583de7bd37c23b1
rupeshhcs-oss <rupeshhcs@gmail.com>
2026-05-17 02:17:28 +0200

    github action changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   reactStrictMode: true,
   swcMinify: true,
+
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+
   env: {
     NEXT_PUBLIC_SITE_NAME: 'Square and Cube',
     NEXT_PUBLIC_SITE_DESCRIPTION: 'Premium Construction Consultancy & Project Management',
